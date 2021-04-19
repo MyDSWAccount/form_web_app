41ea40aedcd8ed5dad6c2fd08091f2278b081698
MyDSWAccount <77691217+MyDSWAccount@users.noreply.github.com>
2021-04-19 09:48:50 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -10,37 +10,40 @@ def render_main():
 def render_first():
     return render_template('pigLatin.html')
 
-@app.route("/response")
+@app.route("/response", methods=['GET', 'POST'])
 def render_response():
-    text_input = request.args['text_input']
-    name_input = ""
-    userId = ""
-    name_input = request.args['name_input']
-    if name_input != "":
-        userId = name_input + "'s"
-    else:
-        userId = "Your"
-    c = 0
-    y = ""
-    txt = []
-    for x in text_input:
-        y = y + x
-        c = c + 1
-        if x == " ":
-            y = y[0:len(y)-1]
-            txt.append(y)
-            y = ""
-        elif c == len(text_input):
-            txt.append(y)
-            y = ""
-    vowel = ["a", "e", "i", "o", "u"]
-    reply = ""
-    for word in txt:
-        if word[0].lower() in vowel:
-            reply = reply + word + "yay" + " "
+    if request.method == 'POST':
+         text_input = request.args['text_input']
+        name_input = ""
+        userId = ""
+        name_input = request.args['name_input']
+        if name_input != "":
+            userId = name_input + "'s"
         else:
-            reply = reply + word[1:len(word)] + word[0:1] + "ay" + " "
-    return render_template('reply.html', response = reply, name = userId)
+            userId = "Your"
+        c = 0
+        y = ""
+        txt = []
+        for x in text_input:
+            y = y + x
+            c = c + 1
+            if x == " ":
+                y = y[0:len(y)-1]
+                txt.append(y)
+                y = ""
+            elif c == len(text_input):
+                txt.append(y)
+                y = ""
+        vowel = ["a", "e", "i", "o", "u"]
+        reply = ""
+        for word in txt:
+            if word[0].lower() in vowel:
+                reply = reply + word + "yay" + " "
+            else:
+                reply = reply + word[1:len(word)] + word[0:1] + "ay" + " "
+        return render_template('reply.html', response = reply, name = userId)
+    else:
+        return render_template('PigLatin.html')
     
 if __name__=="__main__":
     app.run(debug=False, port=54321)
